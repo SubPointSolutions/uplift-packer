a640d3088545e31d6f7b6d8b7a744f1734c1673e
SubPointSupport <support@subpointsolutions.com>
2019-05-07 02:29:52 +1000

    + initial draft for  SharePoint 2019 RTM box

---
+++ packer/scripts/uplift.packer/ms-sharepoint/_sp2019_bin.dsc.ps1
@@ -0,0 +1,50 @@
+﻿
+# fail on errors and include uplift helpers
+$ErrorActionPreference = "Stop"
+
+Import-Module Uplift.Core
+
+Write-UpliftMessage "Running SharePoint prerequisiteinstaller..."
+Write-UpliftEnv
+
+$installDir = Get-UpliftEnvVariable "UPLF_INSTALL_DIR"
+$productKey = Get-UpliftEnvVariable "UPLF_SP_PRODUCT_KEY"
+
+Configuration SP2013_InstallBin
+{
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
+    Import-DscResource -ModuleName SharePointDsc -ModuleVersion "3.3.0.0"
+
+    node "localhost"
+    {
+        SPInstall InstallSharePoint {
+            Ensure = "Present"
+            BinaryDir = $Node.InstallDir
+            ProductKey = $Node.ProductKey
+
+            IsSingleInstance = "Yes"
+        }
+    }
+}
+
+$config = @{
+    AllNodes = @(
+        @{
+            NodeName = 'localhost'
+
+            PSDscAllowDomainUser        = $true
+            PSDscAllowPlainTextPassword = $true
+
+            RetryCount = 10
+            RetryIntervalSec = 30
+
+            InstallDir = $installDir
+            ProductKey = $productKey
+        }
+    )
+}
+
+$configuration = Get-Command SP2013_InstallBin
+Start-UpliftDSCConfiguration $configuration $config
+
+exit 0
---
+++ packer/win-2016-datacenter-sp2019rtm/.build.ps1
@@ -0,0 +1,40 @@
+﻿$ErrorActionPreference = "Stop"
+
+$dirPath    = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+$scriptPath = $MyInvocation.MyCommand.Name
+$packerTemplatesPath = "./../packer_templates"
+
+. "$dirPath/../.build-helpers.ps1"
+
+$templateFileName  = Get-PackerTemplateName($scriptPath)
+
+$coreVaiables      = Get-JSON "$packerTemplatesPath/common/variables.json"
+$coreBuilder       = Get-JSON "$packerTemplatesPath/common/builders-win-2016-vagrant.json"
+# $coreUplift        = Get-JSON "$packerTemplatesPath/common/provisioners-uplift-core.json"
+$corePostProcessor = Get-JSON "$packerTemplatesPath/common/post-processors-win-2016-vagrant.json"
+
+# use 2016 provision with 2019 parameters
+$sp19BinVariables     = Get-JSON "$packerTemplatesPath/win-2016-datacenter-sp2019bin/variables.json"
+$sp19BinProvisioners  = Get-JSON "$packerTemplatesPath/win-2016-datacenter-sp2016bin/provisioners.json"
+
+$specExtractor     = Get-JSON "$packerTemplatesPath/common/provisioners-uplift-box-spec-extractor.json"
+
+$sp19RtmVariables  = Get-JSON "$packerTemplatesPath/win-2016-datacenter-sp2019rtm/variables.json"
+
+$template = @{
+    "builders"        = $coreBuilder.builders
+
+    "variables"       = Merge-Objects `
+                            $coreVaiables.variables `
+                            $sp19BinVariables.variables `
+                            $sp19RtmVariables.variables
+
+    "provisioners"    = Merge-ObjectsAsArray `
+                            $coreBuilder.provisioners `
+                            $sp19BinProvisioners.provisioners `
+                            $specExtractor.provisioners
+
+    "post-processors" = $corePostProcessor.'post-processors'
+}
+
+Save-JSON $template $templateFileName
